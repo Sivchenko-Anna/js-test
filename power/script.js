dc64b9b7e805624660dec9cf284ad138a9d877f3
Sivchenko-Anna <anya.malakhova.99@mail.ru>
2022-08-03 15:34:58 +1000

    add professional script

--- power/script.js
+++ power/script.js
@@ -2,7 +2,27 @@ const inputNode = document.querySelector(".js-input");
 const btnNode = document.querySelector(".js-btn");
 const outputNode = document.querySelector(".js-output");
 
+let state = {
+  title: ''
+}
+
+function changeState (newState) {
+  state = {...newState};
+}
+
+function render () {
+  outputNode.innerHTML = `<h1>${state.title}</h1>`
+}
+
 btnNode.addEventListener("click", function () {
-  outputNode.innerHTML = `<h1>${inputNode.value}</h1>`;
+  changeState({
+    title: inputNode.value
+  });
+  render();
 })
 
+/*
+ Доработанная версия 
+ btnNode.addEventListener("click", function () {
+  outputNode.innerHTML = `<h1>${inputNode.value}</h1>`
+*/
